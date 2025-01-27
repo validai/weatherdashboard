575030aaadc3ea419b4648df9f88903f1ea8083a
validai <djcoast239@gmail.com>
2025-01-27 12:41:07 -0500

    Save updated README, server changes, and additional files

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -9,6 +9,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+console.log('Loaded API Key:', process.env.API_KEY ? 'Exists' : 'Not Found');
+
 // TODO: Serve static files of entire client dist folder
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
